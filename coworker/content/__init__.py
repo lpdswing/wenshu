79cc50f600f09173beb3f4bb6302657525d3f612
lpdswing <lpdswing@qq.com>
2026-07-28 18:15:36 +0800

    feat: model article image plans

--- coworker/content/__init__.py
+++ coworker/content/__init__.py
@@ -1,18 +1,32 @@
 from .article import ArticleValidationError, load_article
 from .hashing import article_text_hash
+from .images import (
+    AssetManifest,
+    AssetPlanError,
+    CoverRequest,
+    IllustrationPlan,
+    IllustrationRequest,
+    parse_asset_plan,
+)
 from .models import ArticleDocument, ArticleFrontmatter, ImageAsset
 from .paths import ContentPathError, resolve_in_roots
 from .review import ArticleReview, prepare_article_review_file
 
 __all__ = [
+    "AssetManifest",
+    "AssetPlanError",
     "ArticleDocument",
     "ArticleFrontmatter",
     "ArticleReview",
     "ArticleValidationError",
     "ContentPathError",
+    "CoverRequest",
     "ImageAsset",
+    "IllustrationPlan",
+    "IllustrationRequest",
     "article_text_hash",
     "prepare_article_review_file",
+    "parse_asset_plan",
     "resolve_in_roots",
     "load_article",
 ]
